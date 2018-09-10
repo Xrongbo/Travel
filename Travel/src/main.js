b57041a87cf3b9f5b1256af6a85d3109bb2e35d3
Xrongbo <38374769+Xrongbo@users.noreply.github.com>
2018-09-10 21:26:08 +0800

    project init

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastckick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastckick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
