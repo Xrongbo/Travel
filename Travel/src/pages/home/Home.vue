9ce9e0d13d192b5c8749d3cc28307c9a3f99e32c
Xrongbo <38374769+Xrongbo@users.noreply.github.com>
2018-09-11 10:08:30 +0800

    change

--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <home-hearder></home-hearder>
+    <home-swiper></home-swiper>
+
   </div>
 </template>
 
 <script>
 import HomeHearder from './components/Header'
+import HomeSwiper from './components/Swiper'
 
 export default {
   name: 'Home',
   components: {
-    HomeHearder
+    HomeHearder,
+    HomeSwiper
   }
 }
 </script>
