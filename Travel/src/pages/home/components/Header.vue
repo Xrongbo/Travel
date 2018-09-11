05644e1780dedf2e3b4daa3b1315a78b793d0bc8
Xrongbo <38374769+Xrongbo@users.noreply.github.com>
2018-09-11 22:11:33 +0800

    city router

--- Travel/src/pages/home/components/Header.vue
+++ Travel/src/pages/home/components/Header.vue
@@ -4,7 +4,9 @@
       <div class="iconfont back-icon">&#xe624;</div>
     </div>
     <div class="header-input"><span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题</div>
+    <router-link to="/city">
     <div class="header-right">{{ this.city }}<span class="iconfont arrow-icon">&#xe64a;</span></div>
+    </router-link>
   </div>
 </template>
 
@@ -19,30 +21,31 @@ export default {
 <style lang="stylus" scoped>
   @import "~styles/varibles.styl"
   .header
-    display: flex
-    line-height: .86rem
-    background-color :$bgColor
+    display flex
+    line-height $headerHeight
+    background-color $bgColor
     .header-left
-      width: .64rem
-      float: left
+      width .64rem
+      float left
       .back-icon
-        text-align: center
-        font-size :.4rem
+        text-align center
+        font-size .4rem
     .header-input
-      flex: 1
-      height :.64rem
-      line-height: .64rem
-      margin-top: .12rem
-      margin-left :.2rem
-      padding-left :.2rem
+      flex 1
+      height .64rem
+      line-height .64rem
+      margin-top .12rem
+      margin-left .2rem
+      padding-left .2rem
       background-color #fff
-      border-radius : .1rem
-      color :#ccc
+      border-radius  .1rem
+      color #ccc
     .header-right
-      width: 1.24rem
-      float: right
-      text-align :center
+      width 1.24rem
+      float right
+      text-align center
+      color #fff
       .arrow-icon
-        margin-left: .04rem
-        font-size :.24rem
+        margin-left .04rem
+        font-size .24rem
 </style>
